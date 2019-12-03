d50f511f9c70d0d553cd882ce75a006091f6102d
J <40594697+midicool@users.noreply.github.com>
2019-12-03 14:52:28 +0900

    Update Turtle.py

--- ColabTurtle/Turtle.py
+++ ColabTurtle/Turtle.py
@@ -306,7 +306,7 @@ def circle(rad):
   r2=rad*rad
   for x in range(-rad,rad):
     y = math.sqrt((r2 - x*x) + 0.5)
-    _moveToNewPosition(x,y)
+    _moveToNewPosition((x, y))
       
 
 # change the width of the lines drawn by the turtle, in pixels
